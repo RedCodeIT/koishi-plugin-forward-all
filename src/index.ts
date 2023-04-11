70c9cab565be57972ed4dfe1755eaab0e2b9d261
wofbi <wofbi1@outlook.com>
2023-04-12 01:20:55 +0800

    增加参数与说明

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import { Context, Dict, Random, Schema } from 'koishi'
 import WebSocket from 'ws';
 
-interface WebSocketPluginOptions {
-  port?: number;
+export const name = 'forward-all'
+
+const descriptions = {
+  enable: '是否开启 WebSocket 转发',
+  port: 'WebSocket 服务端口'
 }
+export interface Config {
+  enable: boolean
+  port: number
+}
+export const Config: Schema<Config> = Schema.object({
+  enable: Schema.boolean().default(false).description(descriptions.enable),
+  port: Schema.number().default(9388).description(descriptions.port),
+})
 
-export default (ctx: Context, options: WebSocketPluginOptions): void => {
-  const server = new WebSocket.Server({ port: options.port || 9088 });
 
+export default (ctx: Context, config: Config): void => {
+  if (!config.enable){
+    console.log('WebSocket server is disabled.');
+    return;
+  }
+  const server = new WebSocket.Server({ port: config.port});
+  console.log(`WebSocket server listening on port ${config.port}.`);
   server.on('connection', (socket) => {
     console.log('WebSocket client connected.');
 
